d9cd935ffd0a59ed689007b39a10c5e7e78d92f4
Newton <newtoljunior@hotmail.com>
2018-06-29 13:50:48 -0300

    Processo de inclusao no banco

--- app/src/main/java/newton/com/roomexample/HomeFragment.java
+++ app/src/main/java/newton/com/roomexample/HomeFragment.java
@@ -6,13 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
+    private Button bnAddUser;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -23,7 +25,21 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        bnAddUser = view.findViewById(R.id.bn_add_user);
+        bnAddUser.setOnClickListener(this);
+
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.bn_add_user:
+                MainActivity.fragmentManager.beginTransaction().replace(R.id.fragment_container,new AddUserFragment()).addToBackStack(null).commit();
+                break;
+        }
+
+    }
 }
